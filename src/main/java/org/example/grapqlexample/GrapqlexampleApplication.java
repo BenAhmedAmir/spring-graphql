d4c0906816332be1fa881ded4989cc8fca71aec1
BenAhmedAmir <benahmedamir@hotmail.fr>
2024-10-15 09:09:46 +0100

    first commit

---
+++ src/main/java/org/example/grapqlexample/GrapqlexampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.grapqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapqlexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapqlexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/grapqlexample/GrapqlexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grapqlexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapqlexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
